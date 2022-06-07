e3dd562b2ea4f1678e918464c0d70200e661d8eb
Cristian Riaño <albeirochunza90@gmail.com>
2022-06-07 11:00:14 -0500

    primer commit

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express= require('express')
+const app= express()
+
+app.get('/',  (req , res)=>{
+    console.log('Peticion recibida')
+    res.send('<h1>hola mundos</h1>')
+})
+
+app.listen(4000,()=>{
+    console.log('Servidor escuchando el el puerto 4000')
+})
